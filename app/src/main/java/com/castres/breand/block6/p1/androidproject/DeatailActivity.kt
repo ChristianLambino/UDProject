d85a39f05480b5d5bf406d5185e99bea80e896ab
ChristianLambino <148464592+ChristianLambino@users.noreply.github.com>
2024-03-02 08:26:32 +0800

    retrofitting

---
+++ app/src/main/java/com/castres/breand/block6/p1/androidproject/DeatailActivity.kt
@@ -0,0 +1,11 @@
+package com.castres.breand.block6.p1.androidproject
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DeatailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deatail)
+    }
+}
